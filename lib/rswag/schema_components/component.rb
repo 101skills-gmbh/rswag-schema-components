fcb7dd1bde815f8459c0a640112446c068222b75
Marten Klitzke <marten@fobizz.com>
2025-08-26 15:33:57 +0200

    chore: add helper method to transform enum keys

--- lib/rswag/schema_components/component.rb
+++ lib/rswag/schema_components/component.rb
@@ -1,32 +1,14 @@
 # frozen_string_literal: true
 
+require "active_support"
+require "active_support/core_ext/string/inflections"
+
 module Rswag
   module SchemaComponents
     module Component
       extend ActiveSupport::Concern
 
       included do
-        class << self
-          attr_accessor :schema_value, :schema_hidden_value, :skip_key_transformation_value
-
-          def schema(schema_definition)
-            self.schema_value = if superclass.respond_to?(:schema_value)
-              parent_schema_definition = superclass.schema_value.deep_dup
-              parent_schema_definition.deeper_merge(schema_definition)
-            else
-              schema_definition
-            end
-          end
-
-          def schema_hidden(flag = false)
-            self.schema_hidden_value = flag
-          end
-
-          def skip_key_transformation(flag = false)
-            self.skip_key_transformation_value = flag
-          end
-        end
-
         def to_schema
           self.class.schema_value
         end
@@ -37,6 +19,32 @@ module Rswag
 
         def skip_key_transformation?
           self.class.skip_key_transformation_value
+        end
+      end
+
+      class_methods do
+        attr_accessor :schema_value, :schema_hidden_value, :skip_key_transformation_value
+
+        def schema(schema_definition)
+          self.schema_value = if superclass.respond_to?(:schema_value)
+            parent_schema_definition = superclass.schema_value.deep_dup
+            parent_schema_definition.deeper_merge(schema_definition)
+          else
+            schema_definition
+          end
+        end
+
+        def schema_hidden(flag = false)
+          self.schema_hidden_value = flag
+        end
+
+        def skip_key_transformation(flag = false)
+          self.skip_key_transformation_value = flag
+        end
+
+        def transform_enum_key(key)
+          key = ActiveSupport::Inflector.underscore(key.to_s)
+          key.parameterize(separator: "_").upcase
         end
       end
     end
--- lib/rswag/schema_components/version.rb
+++ lib/rswag/schema_components/version.rb
@@ -1,5 +1,5 @@
 module Rswag
   module SchemaComponents
-    VERSION = "0.5.1"
+    VERSION = "0.5.2"
   end
 end
